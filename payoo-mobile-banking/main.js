24b39eb8ff4268e87daa5b613f0f768dae78ccbc
web-hasib <hasibulislam9284@gmail.com>
2025-02-25 20:02:06 +0600

    transection section done

--- payoo-mobile-banking/main.js
+++ payoo-mobile-banking/main.js
@@ -1,9 +1,12 @@
 // alert();
+// addMoney sention
 document
   .getElementById("add-money")
   .addEventListener("click", function (event) {
     // alert();
     event.preventDefault();
+    const account= document.getElementById('account-num').value;
+    // const convertedAccount= parseInt(account);
     const amount = document.getElementById("amount").value;
     console.log(amount);
     const convertedAmount = parseFloat(amount);
@@ -19,11 +22,20 @@ document
       const sum = convertedMainBalance + convertedAmount;
       document.getElementById("main-balance").innerText = sum;
       // alert(sum);
+
+      //  transection history
+      const container = document.getElementById("trans-his-container");
+      const p = document.createElement("p");
+      p.innerText = `You Added $ ${convertedAmount}  from ${account}. Remaining Balance: $ ${sum}`;
+      container.appendChild(p);
+
+
     } else {
-      alert('wrong pin');
+      alert("wrong pin");
     }
   });
 
+// cashOut section
 document.getElementById("cash-out").addEventListener("click", function (event) {
   event.preventDefault();
   const pin = document.getElementById("cashout-pin").value;
@@ -33,11 +45,17 @@ document.getElementById("cash-out").addEventListener("click", function (event) {
   const mainBalance = document.getElementById("main-balance").innerText;
   const convertedMainBalance = parseFloat(mainBalance);
   if (convertedPin === 1234) {
-   const sum = convertedMainBalance - convertedAmount;
-   document.getElementById("main-balance").innerText=sum;
+    const sum = convertedMainBalance - convertedAmount;
+    document.getElementById("main-balance").innerText = sum;
+
+    //  transection history
+    const container = document.getElementById("trans-his-container");
+    const p = document.createElement("p");
+    p.innerText = `You Withdrew $ ${convertedAmount}  from your account. Remaining Balance: $ ${sum}`;
+    container.appendChild(p);
   } else {
     alert("Wrong pin");
   }
-  
-
 });
+
+// Transection section
--- payoo-mobile-banking/toggle.js
+++ payoo-mobile-banking/toggle.js
@@ -3,8 +3,15 @@ document.getElementById('cash-out-container').style.display = 'none';
 document.getElementById('add-money-box').addEventListener('click', function(){
     document.getElementById('cash-out-container').style.display = 'none';
     document.getElementById('add-money-container').style.display = 'block';
+    document.getElementById('transection-container').style.display = 'none';
 });
 document.getElementById('cash-out-box').addEventListener('click', function(){
     document.getElementById('cash-out-container').style.display = 'block';
     document.getElementById('add-money-container').style.display = 'none';
+    document.getElementById('transection-container').style.display = 'none';
+});
+document.getElementById('transection-box').addEventListener('click', function(){
+    document.getElementById('cash-out-container').style.display = 'none';
+    document.getElementById('add-money-container').style.display = 'none';
+    document.getElementById('transection-container').style.display = 'block';
 });
